7ce18237600d4afd0de3009a43003ee8486de64e
Maris Erts <7067057+mariserts@users.noreply.github.com>
2022-03-15 13:32:17 +0200

    extended forms to render readability

--- iamheadless_publisher_admin_flat_pages/forms.py
+++ iamheadless_publisher_admin_flat_pages/forms.py
@@ -6,7 +6,7 @@ class FlatPageContentForm(forms.Form):
     language = forms.CharField(max_length=255, widget=forms.widgets.HiddenInput(), initial='')
     title = forms.CharField(max_length=255, initial='')
     slug = forms.SlugField(max_length=255, initial='')
-    content = forms.CharField(widget=forms.widgets.Textarea())
+    content = forms.CharField(widget=forms.widgets.Textarea(attrs={'readability': 'true'}))
     seo_keywords = forms.CharField(max_length=160, initial='', required=False)
     seo_description = forms.CharField(max_length=160, initial='', required=False)
 
